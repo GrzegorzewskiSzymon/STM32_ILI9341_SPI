78e68ac8e05a98da8135abfedb6b41386a74f96c
GrzegorzewskiSzymon <szymon.s.grzegorzewski@gmail.com>
2022-12-28 21:58:14 +0100

    improve code clarity

--- Inc/Libraries/RegistersConfig/RegistersConfig.c
+++ Inc/Libraries/RegistersConfig/RegistersConfig.c
@@ -1,13 +1,39 @@
 /*
- * sysclk.c
+ * RegistersConfig.c
  *
- *  Created on: 12 Dec 2022
+ *  Created on: Dec 28, 2022
  *      Author: Szymon Grzegorzewski
  */
 
 #include <stdint.h>
 #include "stm32g431xx.h"
-#include "sysclk.h"
+
+void GPIOA_Setup()
+{
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+
+
+	//
+	//SPI 1
+	//
+
+	//PA5 as SCK
+	GPIOA->MODER  &=   ~(1<<10); //Alternate function mode
+	GPIOA->AFR[0] |= (5<<20);//AFSEL5[0101] -> AF5
+	GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEED5_0;//Medium Speed
+
+	//PA6 as MISO
+	GPIOA->MODER  &=   ~(GPIO_MODER_MODER6_0);
+	GPIOA->AFR[0] |= (5<<GPIO_AFRL_AFSEL6_Pos);
+
+	//PA7 as MOSI
+	GPIOA->MODER  &=   ~(GPIO_MODER_MODER7_0);
+	GPIOA->AFR[0] |= (5<<GPIO_AFRL_AFSEL7_Pos);
+
+	//PA9 as D/C
+	GPIOA->MODER &= ~(GPIO_MODER_MODER9_1);//General purpose output mode
+
+}
 
 void ClockFrequency_Setup()
 {
---
+++ Inc/Libraries/RegistersConfig/RegistersConfig.h
@@ -0,0 +1,15 @@
+/*
+ * RegistersConfig.h
+ *
+ *  Created on: Dec 28, 2022
+ *      Author: Szymon Grzegorzewski
+ */
+
+#ifndef LIBRARIES_REGISTERSCONFIG_REGISTERSCONFIG_H_
+#define LIBRARIES_REGISTERSCONFIG_REGISTERSCONFIG_H_
+
+
+void GPIOA_Setup();
+void ClockFrequency_Setup();
+
+#endif /* LIBRARIES_REGISTERSCONFIG_REGISTERSCONFIG_H_ */
