72ce758556af17fc50a62ffe01025b8e52d63b1b
GrzegorzewskiSzymon <szymon.s.grzegorzewski@gmail.com>
2023-01-02 20:43:40 +0100

    add basic functions to control ILI9341     with initialization

--- Src/main.c
+++ Src/main.c
@@ -2,6 +2,7 @@
 #include "stm32g431xx.h"
 #include "Libraries/RegistersConfig/RegistersConfig.h"
 #include  "Libraries/SPI/Spi.h"
+#include "Libraries/TFT_ILI9341/TFT_ILI9341.h"
 
 
 int main(void)
@@ -9,10 +10,12 @@ int main(void)
 	ClockFrequency_Setup();//170MHz
 	GPIOA_Setup();
 	Spi1_Setup();
+	Interrupt_Setup();
+	Systick_Setup();
 
 	SPI1_ENABLE;
 
-	Spi1_Send(74); //oscilloscope test
+	ILI9341_Init();
 
 	while(1)
 	{
