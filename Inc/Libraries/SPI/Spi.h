f1f26df6c2413538db838a51ed29e49f7a08cee5
GrzegorzewskiSzymon <szymon.s.grzegorzewski@gmail.com>
2023-01-04 16:56:50 +0100

    improve code clarity

--- Inc/Libraries/SPI/Spi.h
+++ Inc/Libraries/SPI/Spi.h
@@ -9,11 +9,6 @@
 #define LIBRARIES_SPI_SPI_H_
 
 
-
-//PA9 as D/C
-#define SPI1_DC_COMMAND  GPIOA->BSRR |= GPIO_BSRR_BR9; //LOW  - Command
-#define SPI1_DC_DATA 	 GPIOA->BSRR |= GPIO_BSRR_BS9; //HIGH - Data
-
 #define SPI1_ENABLE  SPI1->CR1 |=  SPI_CR1_SPE; //SPI  enable
 #define SPI1_DISABLE SPI1->CR1 &=~ SPI_CR1_SPE; //SPI disable
 
